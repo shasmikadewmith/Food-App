99bd5f6a2a9fa68287615ffeaa4c2da6e5a99838
shasmikadewmith <shasmikadewmith@gmail.com>
2026-05-03 13:08:45 +0530

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # Food-App
 # Food-App
 # Food-App
+# Food-App
--- backend/routes/menuRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMenus, getMenu, createMenu, updateMenu, deleteMenu } = require('../controllers/menuController');
-const { protect, admin } = require('../middleware/auth');
-
-router.route('/').get(getMenus).post(protect, admin, createMenu);
-router.route('/:id').get(getMenu).put(protect, admin, updateMenu).delete(protect, admin, deleteMenu);
-
-module.exports = router;
